81310ce101a278743f9354a80f739abd1158a523
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2016-01-25 00:09:35 +0100

    test: adds additional tests for PUT, DELETE; HEAD and a test for an invalid blacklist

--- test/test.js
+++ test/test.js
@@ -5,6 +5,34 @@ import supertest from 'supertest';
 
 const request = supertest.agent(app.listen());
 
+describe('Using PUT',() => {
+  it('should return a 405 (Method not allowed)', done => {
+    request
+      .put('/')
+      .expect(405)
+      .end(done);
+  });
+});
+
+
+describe('Using DELETE',() => {
+  it('should return a 405 (Method not allowed)', done => {
+    request
+      .delete('/')
+      .expect(405)
+      .end(done);
+  });
+});
+
+describe('Using HEAD',() => {
+  it('should return a 405 (Method not allowed)', done => {
+    request
+      .head('/')
+      .expect(405)
+      .end(done);
+  });
+});
+
 describe('Using GET',() => {
   it('should return a 405 (Method not allowed)', done => {
     request
@@ -22,7 +50,23 @@ describe('Using POST',() => {
       .end(done);
   });
 
-  it('should return a 401 (Bad Request) for a blacklisted user id', done => {
+  it('should return a 400 (Bad Request) if no user id was supplied', done => {
+    request
+      .post('/')
+      .send({ foo: 'bar'})
+      .expect(400)
+      .end(done);
+  });
+
+  it('should return a 400 (Bad Request) for an empty user id', done => {
+    request
+      .post('/')
+      .send({ user_id: ''})
+      .expect(400)
+      .end(done);
+  });
+
+  it('should return a 401 (Unauthorized) for a blacklisted user id', done => {
     request
       .post('/')
       .send({ user_id: '123'})
@@ -37,6 +81,18 @@ describe('Using POST',() => {
     request
       .post('/')
       .send({ user_id: 'not-banned'})
+      .expect(200, {
+        blacklisted: false
+      })
+      .expect('Content-Type', /json/)
+      .end(done);
+  });
+
+  it('should return a 200 (OK) if the banned users file is not found', done => {
+    process.env.BLACKLISTED_USERS_FILE = `${__dirname}/not-existing.txt`;
+    request
+      .post('/')
+      .send({ user_id: '123'})
       .expect(200, {
         blacklisted: false
       })
